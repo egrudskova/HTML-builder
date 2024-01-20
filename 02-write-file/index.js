7d94b6f318af578867308f5502b6f1f544075124
Elina <elina50505051@yandex.ru>
2024-01-21 00:14:44 +0300

    feat: add solution (02-write-file)

---
+++ 02-write-file/index.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const path = require('path');
+const { stdin, stdout } = process;
+
+const output = fs.createWriteStream(path.resolve(__dirname, 'text.txt'));
+stdout.write('Hi, User. Insert your input\n');
+stdin.on('data', (data) => {
+  if (data.toString().trim() === 'exit') {
+    process.exit();
+  }
+  output.write(data.toString().trim());
+});
+
+output.on('error', (err) => console.log('Error', err.message));
+process.on('exit', () => stdout.write('Goodbye'));
